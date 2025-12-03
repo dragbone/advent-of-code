b9bde81393f14341a4c04dbe9b9bba94ae8acaa0
Sascha Scherrer <mail@sascha-scherrer.ch>
2025-12-03 14:57:34 +0100

    Setup

--- src/main/kotlin/Day.kt
+++ src/main/kotlin/Day.kt
@@ -1,14 +1,22 @@
+import kotlin.io.path.Path
+import kotlin.io.path.absolutePathString
+import kotlin.io.path.createFile
+import kotlin.io.path.exists
 import kotlin.time.measureTime
 
 abstract class Day(val day: Int, val year: Int) {
     abstract fun part1(text: String): Any
     abstract fun part2(text: String): Any
+    val exampleFile = "y$year/d${day}_ex.txt"
+    val file = "y$year/d${day}.txt"
     fun run() {
+        Path("src/main/resources", exampleFile).run { if (!exists()) createFile() }
+        Path("src/main/resources", file).run { if (!exists()) createFile() }
         run {
             println("=== PART 1 ===")
             val duration = measureTime {
-                println("Example: ${part1(getText("y$year/d${day}_ex.txt"))}")
-                println("Solution: ${part1(getText("y$year/d$day.txt"))}")
+                println("Example: ${part1(getText(exampleFile))}")
+                println("Solution: ${part1(getText(file))}")
             }
             println("Duration: $duration")
         }
@@ -16,8 +24,8 @@ abstract class Day(val day: Int, val year: Int) {
         run {
             println("=== PART 2 ===")
             val duration = measureTime {
-                println("Example: ${part2(getText("y$year/d${day}_ex.txt"))}")
-                println("Solution: ${part2(getText("y$year/d$day.txt"))}")
+                println("Example: ${part2(getText(exampleFile))}")
+                println("Solution: ${part2(getText(file))}")
             }
             println("Duration: $duration")
         }
