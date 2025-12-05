a6ef5859a3a6f9516c4e3c4febd8779ee8837d6f
Sascha Scherrer <mail@sascha-scherrer.ch>
2025-12-05 11:12:24 +0100

    Day 4

--- src/main/kotlin/y2025/Day4.kt
+++ src/main/kotlin/y2025/Day4.kt
@@ -7,6 +7,54 @@ fun main() {
 }
 
 class Day4 : Day(4, 2025) {
-    override fun part1(text: String): Any = Unit
-    override fun part2(text: String): Any = Unit
+    override fun part1(text: String): Any {
+        val map = text.lines().mapIndexed { y, line ->
+            line.mapIndexed { x, b ->
+                if (b == '@') {
+                    Pair(x, y)
+                } else {
+                    null
+                }
+            }.filterNotNull()
+        }.flatten().toSet()
+        return map.count { pos ->
+            (-1..1).sumOf { y ->
+                (-1..1).count { x ->
+                    Pair(pos.first + x, pos.second + y) in map
+                }
+            } <= 4
+        }
+    }
+
+    override fun part2(text: String): Any {
+        val map = text.lines().mapIndexed { y, line ->
+            line.mapIndexed { x, b ->
+                if (b == '@') {
+                    Pair(x, y)
+                } else {
+                    null
+                }
+            }.filterNotNull()
+        }.flatten().toMutableSet()
+        var count = 0
+        var change: Boolean
+        do {
+            change = false
+            val remove = mutableSetOf<Pair<Int,Int>>()
+            map.forEach { pos ->
+                if ((-1..1).sumOf { y ->
+                        (-1..1).count { x ->
+                            Pair(pos.first + x, pos.second + y) in map
+                        }
+                    } <= 4) {
+                    change = true
+                    count += 1
+                    remove.add(pos)
+                }
+            }
+            map -= remove
+        } while (change)
+
+        return count
+    }
 }
