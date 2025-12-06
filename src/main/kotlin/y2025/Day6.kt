966b91796fa3066d24869c6efa484eda995470bf
Sascha Scherrer <mail@sascha-scherrer.ch>
2025-12-06 08:24:54 +0100

    Day6 part1

--- src/main/kotlin/y2025/Day6.kt
+++ src/main/kotlin/y2025/Day6.kt
@@ -1,6 +1,7 @@
 package y2025
 
 import Day
+import toColumns
 
 fun main() {
     Day6().run()
@@ -8,7 +9,18 @@ fun main() {
 
 class Day6 : Day(6, 2025) {
     override fun part1(text: String): Any {
-        return 0
+        val numColums = text.lines().first().split(" ").count { it.isNotBlank() }
+        val cols = text.lineSequence().toColumns(numColums) {
+            it.trim().split(Regex("""\s+"""))
+        }
+        val sum = cols.sumOf { col ->
+            when (col.last()) {
+                "*" -> col.dropLast(1).fold(1L) { acc, s -> acc * s.toLong() }
+                "+" -> col.dropLast(1).sumOf { it.toLong() }
+                else -> TODO()
+            }
+        }
+        return sum
     }
 
     override fun part2(text: String): Any {
