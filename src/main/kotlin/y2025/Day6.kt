73b8fd9bf20844b1bbddf25e13a669289df53b6a
Sascha Scherrer <mail@sascha-scherrer.ch>
2025-12-06 08:16:17 +0100

    Day6 start

---
+++ src/main/kotlin/y2025/Day6.kt
@@ -0,0 +1,17 @@
+package y2025
+
+import Day
+
+fun main() {
+    Day6().run()
+}
+
+class Day6 : Day(6, 2025) {
+    override fun part1(text: String): Any {
+        return 0
+    }
+
+    override fun part2(text: String): Any {
+        return 0
+    }
+}
